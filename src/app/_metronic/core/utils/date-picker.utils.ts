75c2054895a156ca02802f6d69c282496b2f4c30
Damar Khoirul Huda <damar.huda@javan.co.id>
2021-09-09 12:09:58 +0700

    date and checkbox

--- src/app/_metronic/core/utils/date-picker.utils.ts
+++ src/app/_metronic/core/utils/date-picker.utils.ts
@@ -1,56 +1,68 @@
-import { Injectable } from '@angular/core';
-import { NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import {Injectable} from '@angular/core';
+import {NgbDateAdapter, NgbDateParserFormatter, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 /**
  * This Service handles how the date is represented in scripts i.e. ngModel.
  */
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CustomAdapter extends NgbDateAdapter<string> {
 
-  readonly DELIMITER = '/';
+  readonly DELIMITER = '-';
 
   fromModel(value: string | null): NgbDateStruct | null {
     if (value) {
-      const date = value.split(this.DELIMITER);
+      let date = value.split(this.DELIMITER);
       return {
-        month: parseInt(date[0], 10),
-        day: parseInt(date[1], 10),
-        year: parseInt(date[2], 10)
+        day : parseInt(date[2], 10),
+        month : parseInt(date[1], 10),
+        year : parseInt(date[0], 10)
       };
     }
     return null;
   }
 
   toModel(date: NgbDateStruct | null): string | null {
-    return date ? date.month + this.DELIMITER + date.day + this.DELIMITER + date.year : null;
+    let month = '';
+    if (date) {
+      if (date.month < 10) {
+        month = '0' + date.month.toString();
+      } else {
+        month = date.month.toString();
+      }
+    }
+    return date ? date.year + this.DELIMITER + date.month + this.DELIMITER + date.day : null;
   }
 }
 
 /**
  * This Service handles how the date is rendered and parsed from keyboard i.e. in the bound input field.
  */
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CustomDateParserFormatter extends NgbDateParserFormatter {
 
-  readonly DELIMITER = '/';
+  readonly DELIMITER = '-';
 
   parse(value: string): NgbDateStruct | null {
     if (value) {
-      const date = value.split(this.DELIMITER);
+      let date = value.split(this.DELIMITER);
       return {
-        month: parseInt(date[0], 10),
-        day: parseInt(date[1], 10),
-        year: parseInt(date[2], 10)
+        day : parseInt(date[0], 10),
+        month : parseInt(date[1], 10),
+        year : parseInt(date[2], 10)
       };
     }
     return null;
   }
 
   format(date: NgbDateStruct | null): string {
-    return date ? date.month + this.DELIMITER + date.day + this.DELIMITER + date.year : '';
+    let month = '';
+    if (date) {
+      if (date.month < 10) {
+        month = '0' + date.month.toString();
+      } else {
+        month = date.month.toString();
+      }
+    }
+    return date ? date.day + this.DELIMITER + month + this.DELIMITER + date.year : '';
   }
 }
